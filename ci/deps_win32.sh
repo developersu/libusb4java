d29eb28e4f7b9434ebd5691b566af41f5618ac47
Dmitry Isaenko <developer.su@gmail.com>
2026-09-12 12:45:48 +0300

    temp tests +3

--- ci/deps_win32.sh
+++ ci/deps_win32.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+# LIBUSB_VERSION=v1.0.30
+# curl -L "https://github.com/libusb/libusb/archive/$LIBUSB_VERSION.zip" -o libusb.zip
+# unzip -q libusb.zip
+# cd libusb
+vcpkg install libusb
