3e72ab8df285341f46331000e1ce43a9e513063a
Dmitry Isaenko <developer.su@gmail.com>
2026-09-12 17:50:48 +0300

    temp tests +6

--- ci/deps_win32.sh
+++ ci/deps_win32.sh
@@ -4,5 +4,6 @@
 # unzip -q libusb.zip
 # cd libusb
 vcpkg install libusb
-echo LibUsb_INCLUDE_DIR=C:/vcpkg/packages/libusb_x64-windows/include/  >> $GITHUB_ENV
-echo LibUsb_LIBRARIES=C:/vcpkg/packages/libusb_x64-windows/lib/  >> $GITHUB_ENV
+ls C:\vcpkg\packages\libusb_x64-windows\
+echo 'LibUsb_INCLUDE_DIR=C:\vcpkg\packages\libusb_x64-windows\include\'  >> $GITHUB_ENV
+echo 'LibUsb_LIBRARIES=C:\vcpkg\packages\libusb_x64-windows\lib\'  >> $GITHUB_ENV
